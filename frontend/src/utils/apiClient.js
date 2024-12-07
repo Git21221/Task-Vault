8127ea94b19fe36ff627cf14363f6ef5e0fc805f
Saikat Das <saikatdas40g@gmail.com>
2024-12-07 13:25:20 +0530

    move base_url declaration inside apiClient function for better scope management

--- frontend/src/utils/apiClient.js
+++ frontend/src/utils/apiClient.js
@@ -1,8 +1,8 @@
-const base_url = import.meta.env.VITE_DEVELOPMENT_ENVIRONMENT
-  ? import.meta.env.VITE_DEV_SERVER_BASE_URL
-  : import.meta.env.VITE_PROD_SERVER_BASE_URL;
 
 export const apiClient = async (url, method, options) => {
+  const base_url = import.meta.env.VITE_DEVELOPMENT_ENVIRONMENT
+    ? import.meta.env.VITE_DEV_SERVER_BASE_URL
+    : import.meta.env.VITE_PROD_SERVER_BASE_URL;
   const response = await fetch(`${base_url}/${url}`, {
     method,
     credentials: "include",
